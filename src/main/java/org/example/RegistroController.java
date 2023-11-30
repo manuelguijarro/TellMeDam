af9b72f87c30cf2e38c6d207e2701852133c2d4c
manuelguijarro <manu2506.1994@gmail.com>
2023-11-30 17:23:45 +0100

    registro-commit

--- src/main/java/org/example/RegistroController.java
+++ src/main/java/org/example/RegistroController.java
@@ -2,11 +2,24 @@ package org.example;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class RegistroController {
+    @FXML
+    private TextField nombreUsuarioTextField;
+    @FXML
+    private TextField emailUsuarioTextField;
+    @FXML
+    private TextField contraseniaUsuarioTextField;
 
     @FXML
     private void switchToLogin() throws IOException {
         App.setRoot("login");
+    }@FXML
+    private void registrarse() throws IOException {
+        String nombreUsuario = nombreUsuarioTextField.getText();
+        String emailUsuario = emailUsuarioTextField.getText();
+        String contraseniaUsuario = contraseniaUsuarioTextField.getText();
+
     }
 }
